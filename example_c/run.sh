428ff6cda51079ac493277ef06b401362a53bdd4
Robpol86 <robpol86@gmail.com>
2015-01-04 19:19:51 -0800

    New program: scan_access_points.c.
    Damn that was hard. It's almost done. It just doesn't print SSIDs yet,
    only BSSIDs and frequencies.
    
    Found a helpful debug variable for netlink too. Added it as a comment to
    run.sh.

--- example_c/run.sh
+++ example_c/run.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 set -e
 #export NLDBG=2
+#export NLCB=debug
 
-for f in *.c; do
+for f in list_network_interfaces.c show_wifi_interface.c; do
     echo "Building $f..."
     gcc $(pkg-config --cflags --libs libnl-genl-3.0) -o /tmp/program $f
     echo "Executing..."
     /tmp/program
+    echo
+done
+
+for f in scan_access_points.c; do
+    echo "Building $f..."
+    gcc $(pkg-config --cflags --libs libnl-genl-3.0) -o /tmp/program $f
+    echo "Executing..."
+    sudo /tmp/program
     echo
 done

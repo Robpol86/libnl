da8017b6b29ba0ff89f5d0149e4f76c920e84a88
Robpol86 <robpol86@gmail.com>
2015-01-13 23:45:51 -0800

    Defined nlmsg_alloc(), NLMSG_ALIGN, and related.
    Ported some functions and lambdas (or whatever they're called in C) from
    lib/msg.c and netlink.h.

--- wifipy/netlink/msg.py
+++ wifipy/netlink/msg.py
@@ -7,6 +7,72 @@ License as published by the Free Software Foundation version 2.1
 of the License.
 """
 
+from ctypes import sizeof
+from resource import getpagesize
+
+from wifipy.netlink.netlink import NLMSG_ALIGN, NLMSG_HDRLEN, nlmsghdr
+from wifipy.netlink.types import nl_msg
+
+PAGESIZE = getpagesize()
+
+
+def nlmsg_size(payload):
+    """Calculates size of netlink message based on payload length.
+
+    Modeled after:
+    http://www.infradead.org/~tgr/libnl/doc/api/msg_8c_source.html#l00054
+
+    Positional arguments:
+    payload -- length of payload (integer).
+
+    Returns:
+    Size of netlink message without padding (integer).
+    """
+    return int(NLMSG_HDRLEN + payload)
+
+
+def nlmsg_msg_size(payload):
+    """Calculates size of netlink message based on payload length.
+
+    Modeled after:
+    http://www.infradead.org/~tgr/libnl/doc/api/msg_8c_source.html#l00059
+
+    Positional arguments:
+    payload -- length of payload (integer).
+
+    Returns:
+    Size of netlink message without padding (integer).
+    """
+    return int(nlmsg_size(payload))
+
+
+def nlmsg_total_size(payload):
+    """Calculates size of netlink message including padding based on payload length.
+
+    Modeled after:
+    http://www.infradead.org/~tgr/libnl/doc/api/msg_8c_source.html#l00072
+
+    Positional arguments:
+    payload -- length of payload (integer).
+
+    Returns:
+    Size of netlink message including padding (integer).
+    """
+    return int(NLMSG_ALIGN(nlmsg_msg_size(payload)))
+
 
 def nlmsg_alloc():
-    pass
+    """Allocate a new netlink message with the default maximum payload size.
+
+    Modeled after:
+    http://www.infradead.org/~tgr/libnl/doc/api/msg_8c_source.html#l00261
+
+    Returns:
+    Newly allocated netlink message.
+    """
+    nm = nl_msg()
+    len_ = sizeof(nlmsghdr) if PAGESIZE < sizeof(nlmsghdr) else PAGESIZE
+    nm.nm_refcnt = 1
+    nm.nm_protocol = -1
+    nm.nm_size = len_
+    return nm
--- wifipy/netlink/netlink.py
+++ wifipy/netlink/netlink.py
@@ -7,7 +7,8 @@ License as published by the Free Software Foundation version 2.1
 of the License.
 """
 
-from ctypes import c_uint16, c_uint32, c_ushort, Structure
+from ctypes import c_uint, c_uint16, c_uint32, c_ushort, sizeof, Structure
+
 
 NLM_F_REQUEST = 1  # It is request message.
 NLM_F_MULTI = 2  # Multipart message, terminated by NLMSG_DONE
@@ -15,17 +16,20 @@ NLM_F_ACK = 4  # Reply with ack, with zero or error code
 NLM_F_ECHO = 8  # Echo this request
 NLM_F_DUMP_INTR = 16  # Dump was inconsistent due to sequence change
 
+
 # Modifiers to GET request.
 NLM_F_ROOT = 0x100  # Specify tree root.
 NLM_F_MATCH = 0x200  # Return all matching.
 NLM_F_ATOMIC = 0x400  # Atomic GET.
 NLM_F_DUMP = NLM_F_ROOT | NLM_F_MATCH
 
+
 # Modifiers to NEW request.
 NLM_F_REPLACE = 0x100  # Override existing.
 NLM_F_EXCL = 0x200  # Do not touch, if it exists.
 NLM_F_CREATE = 0x400  # Create, if it does not exist.
 NLM_F_APPEND = 0x800  # Add to end of list.
+
 
 NETLINK_ADD_MEMBERSHIP = 1
 NETLINK_DROP_MEMBERSHIP = 2
@@ -48,3 +52,10 @@ class nlmsghdr(Structure):
         ('nlmsg_seq', c_uint32),  # Sequence number.
         ('nlmsg_pid', c_uint32),  # Sending process port ID.
     ]
+
+
+NLMSG_ALIGNTO = c_uint(4)
+NLMSG_ALIGN = lambda len_: (len_ + NLMSG_ALIGNTO.value - 1) & ~(NLMSG_ALIGNTO.value - 1)
+NLMSG_HDRLEN = NLMSG_ALIGN(sizeof(nlmsghdr))
+NLMSG_LENGTH = lambda len_: len_ + NLMSG_ALIGN(NLMSG_HDRLEN)
+NLMSG_SPACE = lambda len_: NLMSG_ALIGN(NLMSG_LENGTH(len_))

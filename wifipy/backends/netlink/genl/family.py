ac75fd0a0182d711908aae64df477021c53d5dc3
Robpol86 <robpol86@gmail.com>
2015-01-15 18:49:09 -0800

    Started writing tests.
    Wrote C program that prints the expected test values. Also wrote a
    Python test for part of netlink.py. Tests are passing on my Raspberry
    Pi.
    
    Started work on family and object modules.

---
+++ wifipy/backends/netlink/genl/family.py
@@ -0,0 +1,22 @@
+"""Generic Netlink Family (lib/genl/family.c).
+https://github.com/thom311/libnl/blob/master/lib/genl/family.c
+
+Object representing a kernel side registered Generic Netlink family.
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation version 2.1
+of the License.
+"""
+
+from wifipy.backends.netlink.types import genl_family
+
+
+def genl_family_alloc():
+    """Allocate new Generic Netlink family object.
+    https://github.com/thom311/libnl/blob/master/lib/genl/family.c#L181
+
+    Returns:
+    Newly allocated Generic Netlink family object (Structure) or NULL.
+    """
+    return nl_object_alloc()  # TODO
---
+++ wifipy/backends/netlink/object_.py
@@ -0,0 +1,31 @@
+"""Generic Cacheable Object (lib/object.c).
+https://github.com/thom311/libnl/blob/master/lib/object.c
+
+Generic object data type, for inheritance purposes to implement cacheable data types.
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation version 2.1
+of the License.
+"""
+
+from ctypes import POINTER, sizeof
+
+from wifipy.backends.netlink.object_api import nl_object
+
+
+def nl_object_alloc(ops):
+    """Allocate a new object of kind specified by the operations handle.
+    https://github.com/thom311/libnl/blob/master/lib/object.c#L54
+
+    Positional arguments:
+    ops -- cache operations handle.
+
+    Returns:
+    The new object or None.
+    """
+    new = POINTER(nl_object())
+
+    if ops.oo_size < sizeof(POINTER(new)):
+        raise OSError('BUG: ops.oo_size < sizeof(POINTER(new)')
+    # TODO

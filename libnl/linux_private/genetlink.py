601732c877427895810d28fd091362edb6c78af7
Robpol86 <robpol86@gmail.com>
2015-02-07 12:49:27 -0800

    Great Cleanup Part 1: Delete Files
    Starting to clean up project. Removing old code before re-porting to a
    more pythonic code-base. Old code consists of C structs
    (ctypes.Structure) and such.

--- libnl/linux_private/genetlink.py
+++ libnl/linux_private/genetlink.py
@@ -7,9 +7,7 @@ License as published by the Free Software Foundation version 2.1
 of the License.
 """
 
-from ctypes import c_uint8, c_uint16, sizeof, Structure
-
-from libnl.linux_private.netlink import NLMSG_ALIGN, NLMSG_MIN_TYPE
+from libnl.linux_private.netlink import NLMSG_MIN_TYPE
 
 
 GENL_NAMSIZ = 16  # Length of family name.
@@ -17,16 +15,7 @@ GENL_MIN_ID = NLMSG_MIN_TYPE
 GENL_MAX_ID = 1023
 
 
-class genlmsghdr(Structure):
-    """https://github.com/thom311/libnl/blob/master/include/linux-private/linux/genetlink.h#L12"""
-    _fields_ = [
-        ('cmd', c_uint8),
-        ('version', c_uint8),
-        ('reserved', c_uint16),
-    ]
-
-
-GENL_HDRLEN = NLMSG_ALIGN(sizeof(genlmsghdr))
+#GENL_HDRLEN = NLMSG_ALIGN(sizeof(genlmsghdr))
 GENL_ADMIN_PERM = 0x01
 GENL_CMD_CAP_DO = 0x02
 GENL_CMD_CAP_DUMP = 0x04
--- libnl/netlink_private/types.py
+++ libnl/netlink_private/types.py
@@ -7,13 +7,7 @@ License as published by the Free Software Foundation version 2.1
 of the License.
 """
 
-from ctypes import c_char, c_int, c_uint, c_uint16, c_uint32, POINTER, Structure
-
-from libnl.netlink_private.cache_api import nl_cache_ops
-from libnl.hashtable import nl_hash_table
 from libnl.linux_private.netlink import sockaddr_nl
-from libnl.list import nl_list_head
-from libnl.netlink_private.object_api import NLHDR_COMMON
 
 NL_SOCK_BUFSIZE_SET = 1 << 0
 NL_SOCK_PASSCRED = 1 << 1
@@ -82,20 +76,6 @@ class nl_sock(object):
         return -1 if self.socket_instance is None else self.socket_instance.fileno()
 
 
-class nl_cache(Structure):
-    """https://github.com/thom311/libnl/blob/master/include/netlink-private/types.h#L82"""
-    _fields_ = [
-        ('c_items', nl_list_head),
-        ('c_nitems', c_int),
-        ('c_iarg1', c_int),
-        ('c_iarg2', c_int),
-        ('c_refcnt', c_int),
-        ('c_flags', c_uint),
-        ('hashtable', POINTER(nl_hash_table)),
-        ('c_ops', POINTER(nl_cache_ops)),
-    ]
-
-
 class nl_msg(object):
     """https://github.com/thom311/libnl/blob/master/include/netlink-private/types.h#L133"""
 
@@ -107,16 +87,3 @@ class nl_msg(object):
         self.nm_creds = None
         self.nm_nlh = None
         self.nm_refcnt = 0
-
-
-class genl_family(Structure):
-    """https://github.com/thom311/libnl/blob/master/include/netlink-private/types.h#L783"""
-    _fields_ = NLHDR_COMMON + [
-        ('gf_id', c_uint16),
-        ('gf_name', c_char),
-        ('gf_version', c_uint32),
-        ('gf_hdrsize', c_uint32),
-        ('gf_maxattr', c_uint32),
-        ('gf_ops', nl_list_head),
-        ('gf_mc_grps', nl_list_head),
-    ]

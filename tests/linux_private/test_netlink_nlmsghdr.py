e7f444d7b70e417c96298ecdcaa33871b72be8e6
Robpol86 <robpol86@gmail.com>
2015-02-07 22:51:09 -0800

    Done testing nlmsghdr.
    Forgot that in python, sk.s_local.nl_pid is read-only (it's actually a
    class property that always shows the actual port ID from the netlink
    socket if the socket is open).

--- tests/linux_private/test_netlink_nlmsghdr.py
+++ tests/linux_private/test_netlink_nlmsghdr.py
@@ -1,8 +1,6 @@
 import base64
 import socket
 
-import pytest
-
 from libnl.attr import nla_put_u32, nla_put_u64
 from libnl.linux_private.netlink import NETLINK_ROUTE
 from libnl.misc import msghdr
@@ -11,8 +9,7 @@ from libnl.nl import nl_connect, nl_complete_msg, nl_sendmsg
 from libnl.socket_ import nl_socket_alloc, nl_socket_free
 
 
-@pytest.mark.skipif('True')
-def test_no_attrs(tcp_server):
+def test_socket(tcp_server):
     """// gcc $(pkg-config --cflags --libs libnl-genl-3.0) a.c && (nc -l 2000 |base64 &) && sleep 0.1 && ./a.out
     #include <netlink/msg.h>
     struct nl_sock {
@@ -37,9 +34,8 @@ def test_no_attrs(tcp_server):
         nl_connect(sk, NETLINK_ROUTE);
         sk->s_fd = fd;
         sk->s_local.nl_pid = 0;
+        sk->s_seq_next = 0;
         nl_complete_msg(sk, msg);
-        printf("%d == nlh->nlmsg_seq\n", nlh->nlmsg_seq);  // > Unix time.
-        nlh->nlmsg_seq = 1423350947;
         printf("%d == nlh->nlmsg_seq\n", nlh->nlmsg_seq);
 
         struct iovec iov = { .iov_base = nlh, .iov_len = nlh->nlmsg_len };
@@ -55,10 +51,9 @@ def test_no_attrs(tcp_server):
     // 0 == nlh->nlmsg_flags
     // 0 == nlh->nlmsg_seq
     // 0 == nlh->nlmsg_pid
-    // 1423352334 == nlh->nlmsg_seq
-    // 1423350947 == nlh->nlmsg_seq
+    // 0 == nlh->nlmsg_seq
     // Bytes: 16
-    // EAAAAAAABQCjnNZUAAAAAA==
+    // EAAAAAAABQAAAAAAAAAAAA==
     """
     msg = nlmsg_alloc()
     nlh = nlmsg_hdr(msg)
@@ -74,22 +69,43 @@ def test_no_attrs(tcp_server):
     sk.socket_instance = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sk.socket_instance.connect(tcp_server.server.server_address)
     sk.s_local.nl_pid = 0
+    sk.s_seq_next = 0
     nl_complete_msg(sk, msg)
-    assert 1423351063 <= nlh.nlmsg_seq
-    nlh.nlmsg_seq = 1423350947
-    assert 1423350947 == nlh.nlmsg_seq
+    assert 0 == nlh.nlmsg_seq
+    nlh.nlmsg_pid = 0  # sk.s_local.nl_pid is read-only in Python.
 
     iov = bytes(nlh)[:nlh.nlmsg_len]
     hdr = msghdr(msg_iov=iov)
 
     assert 16 == nl_sendmsg(sk, msg, hdr)
     assert 1 == len(tcp_server.data)
-    assert b'EAAAAAAABQCjnNZUAAAAAA==' == base64.b64encode(iov)
-    assert b'EAAAAAAABQCjnNZUAAAAAA==' == base64.b64encode(tcp_server.data[0])
+    assert b'EAAAAAAABQAAAAAAAAAAAA==' == base64.b64encode(iov)
+    assert b'EAAAAAAABQAAAAAAAAAAAA==' == base64.b64encode(tcp_server.data[0])
     nl_socket_free(sk)
 
 
-@pytest.mark.skipif('True')
+def test_seq():
+    msg = nlmsg_alloc()
+    nlh = nlmsg_hdr(msg)
+    assert 16 == nlh.nlmsg_len
+    assert 0 == nlh.nlmsg_type
+    assert 0 == nlh.nlmsg_flags
+    assert 0 == nlh.nlmsg_seq
+    assert 0 == nlh.nlmsg_pid
+
+    sk = nl_socket_alloc()
+    nl_connect(sk, NETLINK_ROUTE)
+    sk.socket_instance.close()
+    sk.s_local.nl_pid = 0
+    nl_complete_msg(sk, msg)
+    assert 1423351063 <= nlh.nlmsg_seq
+    nlh.nlmsg_seq = 1423350947
+    assert 1423350947 == nlh.nlmsg_seq
+    nlh.nlmsg_pid = 0  # sk.s_local.nl_pid is read-only in Python.
+
+    assert b'EAAAAAAABQCjnNZUAAAAAA==' == base64.b64encode(bytes(nlh)[:nlh.nlmsg_len])
+
+
 def test_two_attrs():
     msg = nlmsg_alloc()
     assert 0 == nla_put_u32(msg, 4, 8)
@@ -101,4 +117,14 @@ def test_two_attrs():
     assert 0 == nlh.nlmsg_pid
     assert 0 == nla_put_u64(msg, 5, 17)
     assert 36 == nlh.nlmsg_len
+
+    sk = nl_socket_alloc()
+    nl_connect(sk, NETLINK_ROUTE)
+    sk.socket_instance.close()
+    sk.s_local.nl_pid = 0
+    sk.s_seq_next = 0
+    nl_complete_msg(sk, msg)
+    assert 0 == nlh.nlmsg_seq
+    nlh.nlmsg_pid = 0  # sk.s_local.nl_pid is read-only in Python.
+
     assert b'JAAAAAAABQAAAAAAAAAAAAgABAAIAAAADAAFABEAAAAAAAAA' == base64.b64encode(bytes(nlh)[:nlh.nlmsg_len])

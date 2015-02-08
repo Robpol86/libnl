48768253ce883f251004e64877b738d5e7a9ecc2
Robpol86 <robpol86@gmail.com>
2015-02-07 21:36:53 -0800

    Finished test_print_hunter().
    Fails assertion if there are any print statements in the codebase (tests
    and library).

--- tests/test__meta.py
+++ tests/test__meta.py
@@ -57,11 +57,41 @@ def test_todo_issue_validator():
     assert not todos_with_no_issues
 
 
-@pytest.mark.skipif('True')
 def test_print_hunter():
     """Verifies that there are no print statements in the codebase."""
-    root_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+    root_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
     assert 'tests' in os.listdir(root_directory)
     generator = (os.path.join(r, s) for r, d, f in os.walk(root_directory) for s in f
                  if s.endswith('.py') and not s.startswith('example_'))
-    regex = re.compile(r'')
+    regex_print = re.compile(r'^(.*)(?<!\w)print(\(|\s)(.*)$', re.MULTILINE)
+
+    # Find all potential prints in Python files. May or may not be in strings.
+    potential_prints = set()
+    for file_path in generator:
+        with open(file_path) as f:
+            for line in f:
+                if regex_print.search(line):
+                    potential_prints.add(file_path)
+                    break
+    if not potential_prints:
+        return
+
+    # Perform lexical analysis on the source code and find all valid print statements/function calls.
+    current_line = list()
+    actual_prints = dict()
+    for file_path in potential_prints:
+        with open(file_path) as f:
+            code = f.read(52428800)  # Up to 50 MiB.
+        for token, code_piece in lex(code, get_lexer_by_name('Python')):
+            if code_piece == '\n':
+                current_line = list()  # References new list, doesn't necessarily remove old list.
+                continue
+            current_line.append(code_piece)
+            if (str(token), code_piece) != ('Token.Keyword', 'print'):
+                continue
+            # If this is reached, there is a print statement in the library!
+            if file_path not in actual_prints:
+                actual_prints[file_path] = list()
+            actual_prints[file_path].append(current_line)  # Keeps reference to current list() alive.
+    actual_prints = dict((f, [''.join(l) for l in lst]) for f, lst in actual_prints.items())
+    assert not actual_prints
